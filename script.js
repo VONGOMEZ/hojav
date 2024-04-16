3192991c9ecc5dbb5ee545f0365293b4574b2b86
Steven_Ospina_Gomez <164075912+VONGOMEZ@users.noreply.github.com>
2024-04-16 13:15:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Ejemplo básico de JavaScript
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("El DOM ha sido cargado.");
+document.getElementById('cambiarTema').addEventListener('click', function() {
+    var body = document.body;
+    if (body.classList.contains('tema-claro')) {
+        body.classList.remove('tema-claro');
+        body.classList.add('tema-oscuro');
+        document.getElementById('iconoActual').classList.remove('fa-sun');
+        document.getElementById('iconoActual').classList.add('fa-moon');
+    } else {
+        body.classList.remove('tema-oscuro');
+        body.classList.add('tema-claro');
+        document.getElementById('iconoActual').classList.remove('fa-moon');
+        document.getElementById('iconoActual').classList.add('fa-sun');
+    }
 });
